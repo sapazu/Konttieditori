4f242b40223ace837b81f0fc76c7bccbc49f5163
Samuel Rantatalo <samuel.miska.rantatalo@gmail.com>
2020-09-07 14:34:21 +0300

    Testi summary
    Testikuvaus

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float RotationSpeed = 45;
+
+    public Vector3 camRotation;
+
+    Transform camPivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetEssentials();   
+    }
+
+    void SetEssentials()
+    {
+        camPivot = GameObject.Find("CameraPivot").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetCameraRotation();
+    }
+
+    void SetCameraRotation()
+    {
+        camRotation.x += Input.GetAxisRaw("Vertical") * RotationSpeed * Time.deltaTime;
+
+        camPivot.eulerAngles = camRotation;
+    }
+}
