5288593700022e038094c23eb2a5f61275933138
Samuel Rantatalo <samuel.miska.rantatalo@gmail.com>
2020-09-11 00:00:14 +0300

    Kontti 2.0

---
+++ Assets/Scripts/AutoTiling.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoTiling : MonoBehaviour
+{
+    Renderer rend;
+
+    public GameObject[] wood;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wood = GameObject.FindGameObjectsWithTag("Wood");
+        CorrectTiling();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CorrectTiling()
+    {
+        for (int i = 0; i < wood.Length; i++)
+        {
+            rend = wood[i].GetComponent<Renderer>();
+            Vector2 scale = Vector2.one;
+            scale.x = wood[i].transform.localScale.x;
+            rend.material.mainTextureScale = scale;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    public bool showBlueprint;
+    public bool showInside;
+
+    CameraController camController;
+
+    GameObject kontti;
+    GameObject menu;
+
+    RaycastHit wallHit;
+    public LayerMask mask;
+
+    Transform selectedWall;
+    Transform oldWall;
+
+    void Start()
+    {
+        SetEssentials();
+    }
+
+    void SetEssentials()
+    {
+        camController = GetComponent<CameraController>();
+        menu = GameObject.Find("Menu");
+        menu.SetActive(false);
+        kontti = GameObject.FindGameObjectWithTag("Kontti");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        CastForWall();
+    }
+
+    void CastForWall()
+    {
+        if (Physics.Raycast(camController.camPivot.position, camController.camPivot.forward * -1, out wallHit, 10, mask))
+        {
+            oldWall = selectedWall;
+            selectedWall = wallHit.transform;
+
+            if (oldWall != selectedWall && oldWall != null && showInside)
+            {
+                oldWall.transform.GetComponent<MeshRenderer>().enabled = true;
+                oldWall = selectedWall;
+            }
+            wallHit.transform.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    public void BlueprintButton()
+    {
+        showBlueprint = !showBlueprint;
+
+        if (showBlueprint)
+        {
+            camController.cam.fieldOfView = 30;
+            camController.oldCamrotation = camController.camRotation;
+            camController.camRotation = Vector3.right * 90;
+            kontti.SetActive(false);
+        }
+        else
+        {
+            camController.cam.fieldOfView = 40;
+            camController.camRotation = camController.oldCamrotation;
+            kontti.SetActive(true);
+        }
+    }
+
+    public void MenuButton()
+    {
+        menu.SetActive(!menu.activeSelf);
+    }
+
+    public void ActivateObject(GameObject _object)
+    {
+        GameObject thisToggle = EventSystem.current.currentSelectedGameObject;
+        bool active = thisToggle.GetComponent<Toggle>().isOn;
+        _object.SetActive(active);
+    }
+}
